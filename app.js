8bc82f6b05b416c68d03d7f7921de9d624234833
Ben Dibuz <ben.dibuz@gmail.com>
2019-04-30 07:41:50 -0500

    add semantic grouping, add js file, add gitignore file

---
+++ app.js
@@ -0,0 +1,14 @@
+let webRoot = 'document.getElementById("web-dev")';
+let engRoot = 'document.getElementById("eng")';
+
+document.getElementById("btn-eng").addEventListener('click,' function(){
+    
+    if webRoot.classList.contains('is-hidden'){
+        webRoot.classList.toggle('is-hidden');
+        
+    }
+});
+
+document.getElementById("btn-web").addEventListener('click,' function(){
+    
+});
