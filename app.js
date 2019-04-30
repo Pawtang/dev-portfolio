bfe0bfd748f0706303196c2c34ff8a7a883e90df
Ben Dibuz <ben.dibuz@gmail.com>
2019-04-30 16:01:18 -0500

    add switch function

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-let webRoot = 'document.getElementById("web-dev")';
-let engRoot = 'document.getElementById("eng")';
+const webRoot = 'document.getElementById("web-dev")';
+const engRoot = 'document.getElementById("eng")';
 
-document.getElementById("btn-eng").addEventListener('click,' function(){
-    
-    if webRoot.classList.contains('is-hidden'){
-        webRoot.classList.toggle('is-hidden');
-        
-    }
+document.getElementById("btn-eng").addEventListener('click', function() {
+    toggleButton();
 });
 
-document.getElementById("btn-web").addEventListener('click,' function(){
-    
+document.getElementById("btn-web").addEventListener('click', function(){
+    toggleButton();
 });
+
+function toggleButton(){
+    document.getElementById("btn-eng").classList.toggle('active');
+    document.getElementById("btn-web").classList.toggle('active');
+    document.getElementById("web-dev").classList.toggle('is-hidden');
+    document.getElementById("eng").classList.toggle('is-hidden');
+}
